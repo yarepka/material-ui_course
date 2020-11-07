f453c9f6711156e5122d1bef0e964216086d91bb
Roman Bubnov <yarepka01@gmail.com>
2020-11-07 18:32:07 +0300

    Added Revolution block and responsiveness

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,11 +6,14 @@ import Button from '@material-ui/core/Button';
 import ButtonArrow from '../components/ui/ButtonArrow';
 import Typography from '@material-ui/core/Typography';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 import animationData from '../animations/landinganimation/data';
 import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 import mobileAppsIcon from '../assets/mobileIcon.svg';
 import websitesIcon from '../assets/websiteIcon.svg';
+import revolutionBackground from '../assets/repeatingBackground.svg';
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -84,6 +87,25 @@ const useStyles = makeStyles((theme) => ({
     marginTop: '12em',
     [theme.breakpoints.down('sm')]: {
       padding: 25,
+    },
+  },
+  revolutionBackground: {
+    backgroundImage: `url(${revolutionBackground})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100%',
+    width: '100%',
+  },
+  revolutionCard: {
+    position: 'absolute',
+    boxShador: theme.shadows[10],
+    borderRadius: 15,
+    padding: '10em',
+    [theme.breakpoints.down('sm')]: {
+      padding: '8em 0',
+      borderRadius: 0,
+      width: '100%',
     },
   },
 }));
@@ -255,6 +277,47 @@ export default function LandinPage() {
               src={websitesIcon}
             />
           </Grid>
+        </Grid>
+      </Grid>
+
+      {/* ----- Revolution ----- */}
+      <Grid item>
+        <Grid
+          container
+          style={{ height: '100em', marginTop: '12em' }}
+          alignItems='center'
+          justify='center'
+        >
+          <Card className={classes.revolutionCard}>
+            <CardContent>
+              <Grid
+                container
+                direction='column'
+                style={{ textAlign: 'center' }}
+              >
+                <Grid item>
+                  <Typography variant='h3' gutterBottom>
+                    The Revolution
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant='subtitle1'>
+                    Visionary insights coupled with cutting-edge technology is a
+                    recipe for revolution.
+                  </Typography>
+                  <Button variant='outlined' className={classes.learnButton}>
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow
+                      width={10}
+                      height={10}
+                      fill={theme.palette.common}
+                    />
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+          <div className={classes.revolutionBackground} />
         </Grid>
       </Grid>
     </Grid>
